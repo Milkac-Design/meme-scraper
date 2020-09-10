cacd9bff03682f7f7213f6f81ef985dd4048cd4e
Milkac-Design <milkac88@gmail.com>
2020-09-10 13:25:31 +0200

    fix create 'meme' folder

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fs = require("fs");
 const Path = require("path");
 
 
+fs.mkdir("./memes", function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("New directory successfully created.")
+    }
+  })
+
 
   const mainUrl = `https://memegen.link/examples`;
 
@@ -28,25 +36,9 @@ const Path = require("path");
     const indexValue8 = 7;
     const indexValue9 = 8;
     const indexValue10 = 9;
-    console.log(`Your memes are:\n https://memegen.link/${urlMeme[indexValue1].attribs.src}
-    https://memegen.link/${urlMeme[indexValue2].attribs.src}
-    https://memegen.link/${urlMeme[indexValue3].attribs.src}
-    https://memegen.link/${urlMeme[indexValue4].attribs.src}
-    https://memegen.link/${urlMeme[indexValue5].attribs.src}
-    https://memegen.link/${urlMeme[indexValue6].attribs.src}
-    https://memegen.link/${urlMeme[indexValue7].attribs.src}
-    https://memegen.link/${urlMeme[indexValue8].attribs.src}
-    https://memegen.link/${urlMeme[indexValue9].attribs.src}
-    https://memegen.link/${urlMeme[indexValue10].attribs.src}`);
+    console.log(`Downloaded 10 memes`);
 
 
-
-// fs.writeFile( "meme1.jpg" ,`https://api.memegen.link/images/${urlMeme[indexValue2].attribs.src}`, function (err) { 
-//     if (err)
-// console.log(err);
-//     else
-// console.log('Append operation complete.');
-// });
 
 async function downloadImage1 () {  
     const url = `https://memegen.link/${urlMeme[indexValue1].attribs.src}`
